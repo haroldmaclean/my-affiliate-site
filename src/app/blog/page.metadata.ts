183e4326fd1cfc4eef122e42e7241e3e7762d3cf
Harold <haroldmacleanb@gmail.com>
2025-04-20 00:01:57 -0700

    Remove unused metadata file to fix Vercel build

--- src/app/blog/page.metadata.ts
+++
@@ -1,6 +0,0 @@
-/*import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Daily Use Gadgets I Can’t Live Without",
-  description: "Gadgets that improve your everyday life.",
-};*/
